a5e5b33421ac7a6d08c8a5aed6bf37e480310288
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-02 10:51:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 
 class Odd extends React.Component {
   componentWillUnmount() {
-    console.log("Even is unmounted");
+    console.log("Odd is unmounted");
   }
   render() {
     return <div id="odd">I am odd</div>;
@@ -12,7 +12,7 @@ class Odd extends React.Component {
 
 class Even extends React.Component {
   componentWillUnmount() {
-    console.log("Odd is unmounted");
+    console.log("Even is unmounted");
   }
   render() {
     return <div id="even">I am even</div>;
